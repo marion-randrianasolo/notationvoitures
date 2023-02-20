ea517eda03f0e7757c80f6f8963f1fb3477bda6f
Marion Randrianasolo <marion.randrianasolo@etu.umontpellier.fr>
2023-02-20 17:24:45 +0100

    update

--- mystite/asgi.py
+++ mystite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for notationvoitures project.
+ASGI config for mystite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notationvoitures.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mystite.settings')
 
 application = get_asgi_application()
--- mystite/wsgi.py
+++ mystite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for notationvoitures project.
+WSGI config for mystite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notationvoitures.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mystite.settings')
 
 application = get_wsgi_application()
